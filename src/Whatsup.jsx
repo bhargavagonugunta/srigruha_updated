06113a52ab285c47caa92751a989cc56d7bfc77f
Bhargva <bhargavagonugunta123@gmail.com>
2024-06-28 13:31:01 +0530

    whats app chaingers

--- src/Whatsup.jsx
+++ src/Whatsup.jsx
@@ -4,7 +4,7 @@ const Whatsup = () => {
   return (
     <div className=" fixed bottom-0 right-0 p-3  z-50 ">
       <a
-        href="https://wa.me/9155536665?text=Hello How Can i help you ?"
+        href="https://wa.me/9248751345?text=Hello How Can i help you ?"
         target="_blank"
       >
         <img src={ImageLogo} width="60" />
